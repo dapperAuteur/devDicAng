dbea447254286f9b49a481e19b729d4da304861c
awe <aweful@awews.com>
2017-02-22 16:39:53 -0700

    create first component that has controller inside

--- src/main/resources/static/js/app/blog-list/blog-list.component.js
+++ src/main/resources/static/js/app/blog-list/blog-list.component.js
@@ -4,7 +4,24 @@
 'use strict';
 
 angular.module('blogList').
-	controller('BlogListCongroller', function(){
+component('blogList', {
+	template: '<div class="">{{title}}</div>' +
+		'<button ng-click="someClickTest()">click me</button>',
+	controller: function($scope){
 		console.log("hello");
-	});
+		$scope.title = "hi there"
+		$scope.someClickTest = function(){
+			console.log("clicked")
+			$scope.title = "clicked"
+		}
+	}
+});
+//	controller('BlogListController', function($scope){
+//		console.log("hello");
+//		$scope.title = "hi there"
+//		$scope.someClickTest = function(){
+//			console.log("clicked")
+//			$scope.title = "clicked"
+//		}
+//	});
 //	component('blogList');
