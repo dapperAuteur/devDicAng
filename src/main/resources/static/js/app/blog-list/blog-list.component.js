78a6b2b2b56d734bd9cc091be28f737d664b563d
awe <aweful@awews.com>
2017-02-22 17:14:48 -0700

    get list of items to appear on template page

--- src/main/resources/static/js/app/blog-list/blog-list.component.js
+++ src/main/resources/static/js/app/blog-list/blog-list.component.js
@@ -4,24 +4,30 @@
 'use strict';
 
 angular.module('blogList').
-component('blogList', {
-	
-	templateUrl: '/templates/blog-list.html',
-	controller: function($scope){
-		console.log("hello");
-		$scope.title = "hi there"
-		$scope.someClickTest = function(){
-			console.log("clicked")
-			$scope.title = "clicked"
+	component('blogList', {
+		templateUrl: '/templates/blog-list.html',
+		controller: function($scope){
+			var blogItems = [
+				{title: "Some Title", id: 1, description: "This is a book"},
+				{title: "Another Title", id: 2, description: "This is a book"},
+				{title: "One More Title", id: 3, description: "This is a book"},
+				{title: "Lastfd Title", id: 4, description: "This is a book"},
+			]
+			$scope.items = blogItems;
+			console.log("hello");
+			$scope.title = "hi there"
+			$scope.someClickTest = function(){
+				console.log("clicked")
+				$scope.title = "clicked"
+			}
 		}
-	}
-});
-//	controller('BlogListController', function($scope){
-//		console.log("hello");
-//		$scope.title = "hi there"
-//		$scope.someClickTest = function(){
-//			console.log("clicked")
-//			$scope.title = "clicked"
-//		}
-//	});
-//	component('blogList');
+	});
+	//	controller('BlogListController', function($scope){
+	//		console.log("hello");
+	//		$scope.title = "hi there"
+	//		$scope.someClickTest = function(){
+	//			console.log("clicked")
+	//			$scope.title = "clicked"
+	//		}
+	//	});
+	//	component('blogList');
