6a4e89819ee1f136e730f30f8363f7aeef733073
awe <aweful@awews.com>
2017-02-24 16:19:38 -0700

    add comments for what is console.log for each link on clickConfirm

--- target/classes/static/js/app/utils/confirm-click/confirm-click.directive.js
+++ target/classes/static/js/app/utils/confirm-click/confirm-click.directive.js
@@ -6,10 +6,12 @@
 angular.module('confirmClick').
 	directive('confirmClick', function(){
 		return {
-			link: function(scope, element, attr){
-				console.log(scope)
-				console.log(element)
-				console.log(attr)
+			link: function(scope, element, attr){//console logs for each link
+				console.log(scope)//gives ChildScope{} object
+				console.log(element)//gives '[a.ng-binding]'
+				console.log(attr.confirmClick)//gives nothing
+				console.log(attr.ngHref)// gives '/blog/1'
+				console.log(attr.href)//gives 'undefined'
 			}
 		}
 	})
