d67242de16025fded861701a3ac1f8dd0a01c13e
awe <aweful@awews.com>
2017-02-24 16:35:57 -0700

    refactor confirmClick directive and blog-list template

--- src/main/resources/static/js/app/utils/confirm-click/confirm-click.directive.js
+++ src/main/resources/static/js/app/utils/confirm-click/confirm-click.directive.js
@@ -6,6 +6,12 @@
 angular.module('confirmClick').
 	directive('confirmClick', function(){
 		return {
+			
+			scope: {
+				message: "@message",
+				post: "=post"
+			},
+			template: "<a ng-href='/blog/{{ post.id }}'>{{ post.title }}</a>",
 			link: function(scope, element, attr){//console logs for each link
 				console.log(scope)//gives ChildScope{} object
 				console.log(element)//gives '[a.ng-binding]'
