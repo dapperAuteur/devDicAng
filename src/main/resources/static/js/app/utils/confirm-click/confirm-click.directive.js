4db0342e977bd0b12dc8409b5f97a6fbb79a4249
awe <aweful@awews.com>
2017-02-24 16:42:49 -0700

    add confirm click functionality to links with alert

--- src/main/resources/static/js/app/utils/confirm-click/confirm-click.directive.js
+++ src/main/resources/static/js/app/utils/confirm-click/confirm-click.directive.js
@@ -13,11 +13,17 @@ angular.module('confirmClick').
 			},
 			template: "<a ng-href='/blog/{{ post.id }}'>{{ post.title }}</a>",
 			link: function(scope, element, attr){//console logs for each link
-				console.log(scope)//gives ChildScope{} object
-				console.log(element)//gives '[a.ng-binding]'
-				console.log(attr.confirmClick)//gives nothing
-				console.log(attr.ngHref)// gives '/blog/1'
-				console.log(attr.href)//gives 'undefined'
+				var msg = scope.message || "Are you sure?";
+				element.bind("click", function(event){
+					if(window.confirm(msg)){
+						console.log('/blog/' + scope.post.id);
+					}
+				})
+				console.log(scope);//gives ChildScope{} object
+				console.log(element);//gives '[a.ng-binding]'
+				console.log(attr.confirmClick);//gives nothing
+				console.log(attr.ngHref);// gives '/blog/1'
+				console.log(attr.href);//gives 'undefined'
 			}
 		}
 	})
